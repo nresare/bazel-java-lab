95d05dce2cf127466c2fb3183033839735ed5f81
Noa Resare <noa@resare.com>
2021-11-12 12:01:03 +0000

    Actually do someting with vavr in Hello World

--- src/main/java/com/resare/lab/Hello.java
+++ src/main/java/com/resare/lab/Hello.java
@@ -4,7 +4,8 @@ import io.vavr.collection.List;
 
 public class Hello {
     public static void main(String[] args) {
-        var list = List.of("a", "b");
-        System.out.println("Hello, world!");
+        var s = List.of("Hello", "world")
+                .foldLeft("", (agg, cur) -> "".equals(agg) ? cur : agg + ", " + cur);
+        System.out.println(s);
     }
 }
