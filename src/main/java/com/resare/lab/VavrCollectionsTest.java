7ba050b9aedd50e08d239082d97d61215dc66cdc
Noa Resare <noa@resare.com>
2022-06-14 12:09:20 +0100

    Added some varv collections tests

--- src/main/java/com/resare/lab/VavrCollectionsTest.java
+++ src/main/java/com/resare/lab/VavrCollectionsTest.java
@@ -1,9 +1,14 @@
 package com.resare.lab;
 
+import io.vavr.collection.HashMap;
 import io.vavr.collection.HashSet;
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VavrCollectionsTest {
@@ -13,5 +18,45 @@ public class VavrCollectionsTest {
 
         var projectSet = HashSet.ofAll(projects);
         assertTrue(projectSet.contains("b"));
+    }
+
+    @Test
+    void testRemoveUndefined() {
+        assertEquals(
+                List.of("a", "b"),
+                List.of(Option.of("a"), Option.none(), Option.of("b"))
+                        //.filter(Option::isDefined).map(Option::get));
+                        .flatMap(Function.identity())
+        );
+    }
+
+    private static class SFunction implements Function<String, String> {
+
+        @Override
+        public String apply(String s) {
+            return s.toUpperCase();
+        }
+    }
+    @Test
+    void testParmeterizedType() {
+        Function<String, String> stringStringFunction = String::toUpperCase;
+
+        var v = List.of("a", "b").map(new SFunction());
+    }
+
+    record ContainsOptionalBool(Option<Boolean> bool) {}
+
+    @Test
+    void testOptionFlatMap() {
+        var maybe = Option.of(new ContainsOptionalBool(Option.none()));
+        var result = maybe.flatMap(ContainsOptionalBool::bool);
+        assertEquals(Option.none(), result);
+    }
+
+    @Test
+    void testMergeMaps() {
+        var value = List.of(HashMap.of("a", "foo"), HashMap.of("b", "bar"));
+        var flattened = value.flatMap(HashMap::iterator).collect(HashMap.collector());
+        assertEquals(flattened, HashMap.of("a", "foo", "b", "bar"));
     }
 }
