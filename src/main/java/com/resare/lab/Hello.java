4b19c8f6089b7b511467c1526ed2cdd7568952c4
Noa Resare <noa@resare.com>
2022-08-12 12:24:19 +0100

    remove new syntax, update bazel version to 5.2.0

--- src/main/java/com/resare/lab/Hello.java
+++ src/main/java/com/resare/lab/Hello.java
@@ -4,7 +4,7 @@ import io.vavr.collection.List;
 
 public class Hello {
     public static void main(String[] args) {
-        var s = List.of("Hello", "world")
+        String s = List.of("Hello", "world")
                 .foldLeft("", (agg, cur) -> "".equals(agg) ? cur : agg + ", " + cur);
         System.out.println(s);
     }
